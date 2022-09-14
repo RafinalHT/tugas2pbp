137aebdf90f984e2465d772e6343fece5e8296d9
Rafinal Haryokusumo Taloputra <89496855+RafinalHT@users.noreply.github.com>
2022-09-14 23:49:48 +0700

    Update views.py

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+def show_katalog(request):
+  data_katalog = CatalogItem.objects.all()
+  context = {
+    'list_katalog': data_katalog,
+    'name': 'Rafinal Haryokusumo Taloputra',
+    'id': '2106634540',
+  }
+  return render(request, "katalog.html", context)
